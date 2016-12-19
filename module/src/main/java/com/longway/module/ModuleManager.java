fe0562705bc68cec93d8913f679e6bfdf98db6d6
longwei01 <longwei01@zuche.com>
2016-12-19 16:19:02 +0800

    add can remove cached module and pure and optimize code

--- module/src/main/java/com/longway/module/ModuleManager.java
+++ module/src/main/java/com/longway/module/ModuleManager.java
@@ -7,15 +7,13 @@ import java.lang.reflect.Constructor;
 import java.util.HashMap;
 
 /**
- * Created by longway on 16/11/6. Email:longway1991117@sina.com
- * Module manager
- * 1.build module service instance
- * 2.cache service instance
- * 3.and so on
+ * Created by longway on 16/11/6. Email:longway1991117@sina.com Module manager 1.build module
+ * service instance 2.cache service instance 3.and so on
  */
 
 public class ModuleManager {
     private static volatile ModuleManager sInstance;
+    private static final Object LOCK = new Object();
     private HashMap<String, Module> mModuleCache;
 
     private ModuleManager() {
@@ -24,15 +22,35 @@ public class ModuleManager {
 
     /**
      * init operator for example perLoad service class or other
+     *
      * @param context global
      */
     public void init(Context context) {
 
     }
 
+    /**
+     * remove cached module
+     * @param moduleName
+     */
+    public void clearModule(String moduleName) {
+        synchronized (mModuleCache) {
+            mModuleCache.remove(moduleName);
+        }
+    }
+
+    /**
+     * clear cache
+     */
+    public void clearAll() {
+        synchronized (mModuleCache) {
+            mModuleCache.clear();
+        }
+    }
+
     public static ModuleManager getInstance() {
         if (sInstance == null) {
-            synchronized (ModuleManager.class) {
+            synchronized (LOCK) {
                 if (sInstance == null) {
                     sInstance = new ModuleManager();
                 }
@@ -43,8 +61,6 @@ public class ModuleManager {
 
     /**
      * load module
-     * @param moduleName
-     * @return
      */
     public Module loadModule(String moduleName) {
         if (TextUtils.isEmpty(moduleName)) {
@@ -54,23 +70,24 @@ public class ModuleManager {
             if (mModuleCache.containsKey(moduleName)) {
                 return mModuleCache.get(moduleName);
             }
-            try {
-                Class<?> clz = Class.forName(moduleName);
-                Constructor constructor = clz.getConstructor();
-                if (constructor == null) {
-                    throw new IllegalModuleTypeException(moduleName + " must be has (void) constructor.");
-                }
-                Object o = clz.newInstance();
-                if (!(o instanceof Module)) {
-                    throw new IllegalModuleTypeException(moduleName + " must be implement Module interface.");
-                }
-                Module module = (Module) o;
-                mModuleCache.put(moduleName, module);
-                return module;
-            } catch (Throwable e) {
-                e.printStackTrace();
-                throw new IllegalModuleTypeException(moduleName+" not create instance.");
+        }
+        try {
+            Class<?> clz = Class.forName(moduleName);
+            if (!Module.class.isAssignableFrom(clz)) {
+                throw new IllegalModuleTypeException(moduleName + " must be isAssignableFrom Module.");
             }
+            Constructor constructor = clz.getConstructor();
+            if (constructor == null) {
+                throw new IllegalModuleTypeException(moduleName + " must be has (void) constructor.");
+            }
+            Module module = (Module) clz.newInstance();
+            synchronized (mModuleCache) {
+                mModuleCache.put(moduleName, module);
+            }
+            return module;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            throw new IllegalModuleTypeException(moduleName + " not create instance.");
         }
     }
 }
